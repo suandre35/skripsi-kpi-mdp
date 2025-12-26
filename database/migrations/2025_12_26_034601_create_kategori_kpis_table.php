ea3c8cbb5f5e2bc9131b66a19edf00f7eb107e30
suandre35 <djalisuandre123654@gmail.com>
2025-12-26 10:57:11 +0700

    Add migration and Models for Indikator and Kategori

---
+++ database/migrations/2025_12_26_034601_create_kategori_kpis_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kategori_kpis', function (Blueprint $table) {
+            $table->id('id_kategori');
+            $table->string('nama_kategori');
+            $table->text('deskripsi')->nullable();
+            $table->enum('status', ['Aktif','Nonaktif'])->default('aktif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kategori_kpis');
+    }
+};
---
+++ database/migrations/2025_12_26_035208_create_indikator_kpis_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('indikator_kpis', function (Blueprint $table) {
+            $table->id('id_indikator');
+            $table->foreignId('nama_kategori')->constrained('kategori_kpis', 'id_kategori')->onDelete('cascade');
+            $table->string('nama_indikator');
+            $table->text('deskripsi')->nullable();
+            $table->string('satuan_pengukuran')->nullable();
+            $table->enum('status', ['Aktif','Nonaktif'])->default('Aktif');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('indikator_kpis');
+    }
+};
